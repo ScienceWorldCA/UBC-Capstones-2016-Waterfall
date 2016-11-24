f4055bc277806d1fdee0e9543f2e97e2d8b8debb
kumowsy <angley_y@hotmail.com>
2016-11-23 16:25:58 -0800

    Update sd_card.ino
    update pattern to 2d array

--- controller/src/sd_card/sd_card.ino
+++ controller/src/sd_card/sd_card.ino
@@ -5,6 +5,7 @@
 const int chipSelect = 8;
 //const int chipSelect = 4; //for Ethernet shield
 const int imageWidthBytes = 8;
+const int lineWidth = 65;
 
 void setup() {
   // put your setup code here, to run once:
@@ -56,34 +57,42 @@ void convertToByte(){
     Serial.println("Error on opening the file");
     return;
   }
+  int lines = dataFile.size() / lineWidth;
   int bitCounter = 8;
   int regCounter = 0;
-  byte shiftReg[imageWidthBytes] = {0};
+  int lineCounter = 0;
+  byte shiftReg[lines][imageWidthBytes] = {0};
   byte temp;
-  while(dataFile.available()){
+  while(lineCounter < lines){
     temp = dataFile.read();
     if(temp == '0'){
       bitCounter--;
-      shiftReg[regCounter] = shiftReg[regCounter] << 1;
+      shiftReg[lineCounter][regCounter] = shiftReg[lineCounter][regCounter] << 1;
     }
     else if(temp == '1'){
       bitCounter--;
-      shiftReg[regCounter] = (shiftReg[regCounter] << 1) + 1;
+      shiftReg[lineCounter][regCounter] = (shiftReg[lineCounter][regCounter] << 1) + 1;
     }
     if(bitCounter == 0){
-      Serial.print(regCounter);
-      Serial.print(':');
-      Serial.print(shiftReg[regCounter], BIN);
-      Serial.print('\t');
       bitCounter = 8;
       regCounter++;
     }
     if(regCounter == 8){
-      Serial.print('\n');
       regCounter = 0;
+      lineCounter++;
     }
   }
   dataFile.close();
+  int i, j;
+  for(i = 0; i < lines; i++){
+    for(j = 0; j < imageWidthBytes; j++){
+      Serial.print(j);
+      Serial.print(':');
+      Serial.print(shiftReg[i][j], BIN);
+      Serial.print('\t');
+    }
+    Serial.print('\n');
+  }
 }
 
 void loop() {
