dc312df95075131c01d80bcee8bd43c7b4686a33
Daniel Sallaway <dsallaway@telus.net>
2017-02-09 11:14:40 -0800

    -Added logic for offsetting high->low and low->high output transitions  to account for valve switching time -A refactor of the entire output code will be made within the next few days

--- controller/src/sd_ethernet/sd_ethernet.ino
+++ controller/src/sd_ethernet/sd_ethernet.ino
@@ -26,16 +26,87 @@ int serial_pins[NUM_VALVE_BANKS] = {SER1_PIN, SER2_PIN};
 //byte shiftReg[16][2] = {0};
 byte *shiftReg;
 
+//TODO: initialize *previous_pattern_line to a constant line representing all valves open (no pattern)
+byte *previous_pattern_line;
 int current_pattern_line = 0;
+
+//counter used for determining when to update output, and for handling delay between writing high->low and low->high transitions
+//to account for valve switching time
+int output_timer_counter;
+
+//The percentage of the the total bit time to delay the writing of low->high transitions
+//TODO: switch this to an absolute time (in ms) perhaps, and use calculations to convert to a number of interrupt calls
+int valve_switch_time_offset = 25;
+
+//TODO: combine into one flag
+byte pattern_load_high_low;
+byte pattern_load_low_high;
 
 void update_output(void)
 {
-  //Update the output with the data stored in the shift register
-  digitalWrite(RCLK_PIN, HIGH);
-  digitalWrite(RCLK_PIN, LOW); 
+  //do the high->low transition early then tell main to load the low->high transition
+  if (output_timer_counter == 0)
+  {
+    Serial.println("High->low\r\n");
+    
+    //Update the output with the data stored in the shift register
+    digitalWrite(RCLK_PIN, HIGH);
+    digitalWrite(RCLK_PIN, LOW);
 
+    pattern_load_low_high = 1;
+  }
+  //do the low->high transition early then tell main to load the high->low transition 
+  else if (output_timer_counter == valve_switch_time_offset)
+  {
+    Serial.println("Low->High\r\n");
+    
+    //Update the output with the data stored in the shift register
+    digitalWrite(RCLK_PIN, HIGH);
+    digitalWrite(RCLK_PIN, LOW);
+
+    pattern_load_high_low = 1;
+  }
+
+  //TODO: add constant for # of interrupts per bit cycle
+  output_timer_counter = (output_timer_counter + 1) % 100;
+}
+
+//Switching of valves from open to closed must occur earlier due to switching time of valves
+void load_shiftreg_high_low ()
+{
+  
   //Update the data stored in the shift register to be ready for the next line as soon as the timer triggers
   current_pattern_line = (current_pattern_line + 1) % PATTERN_LENGTH;
+  
+  
+  //Iterate through the 8 bits being sent to a shift register for a given row (prepare the next row for output)
+  int j;
+  for (j = 0; j < VALVES_PER_VALVE_BANK; j++)
+  {
+    //Iterate through the shift registers, sending one bit to each
+    int k;
+    for (k = 0; k < NUM_VALVE_BANKS; k++)
+    {
+      //If current output bit is 1 AND previous output bit is 1, output 1 again
+      //Otherwise, if value is staying 0 or switching 1 to 0 output 0
+      //If we are switching high to low, delay this output to account for valve switching time
+      if ((shiftReg[(current_pattern_line * NUM_VALVE_BANKS) + k] >> j) & 1 && (previous_pattern_line[k] >> j) & 1)
+        digitalWrite(serial_pins[k], HIGH);
+      else
+        digitalWrite(serial_pins[k], LOW);
+    }
+    //Write this set of bits into the shift registers (1 per shift register)
+    digitalWrite(SRCLK_PIN, HIGH);
+    digitalWrite(SRCLK_PIN, LOW);
+  }
+
+  pattern_load_high_low = 0;
+}
+
+//Switching of valves from closed to open must occur later due to switching time of valves
+void load_shiftreg_low_high ()
+{
+  //Update the data stored in the shift register to be ready for the next line as soon as the timer triggers
   
   //Iterate through the 8 bits being sent to a shift register for a given row (prepare the next row for output)
   int j;
@@ -54,10 +125,18 @@ void update_output(void)
     digitalWrite(SRCLK_PIN, HIGH);
     digitalWrite(SRCLK_PIN, LOW);
   }
+  //Update the previous_pattern_line to contain the current line
+  previous_pattern_line = &shiftReg[current_pattern_line];
+
+  pattern_load_low_high = 0;
 }
 
 void setup_shiftregs() {
-  MsTimer2::set(250, (&update_output)); // initialize timer to trigger update_output(), and set a 250 ms period
+  output_timer_counter = 0;
+
+  //20ms multiplied by 100 interrupts/bit = 2000ms
+  //TODO: make this more... readable
+  MsTimer2::set(20, (&update_output)); // initialize timer to trigger update_output(), and set a 250 ms period
   MsTimer2::start();
 
   digitalWrite(SRCLR_PIN, HIGH); //Disable Serial Clear
@@ -191,5 +270,12 @@ void convertToByte(){
 void loop(){
   //convertToByte();
   setup_shiftregs();
-  while(1);
+  previous_pattern_line = shiftReg;
+  while(1)
+  {
+    if (pattern_load_high_low)
+      load_shiftreg_high_low();
+    if (pattern_load_low_high)
+      load_shiftreg_low_high();
+  }
 } 
